be3e4760aca1ed2fc890d12be24753228fb82d16
Alfon5000 <alfonlai97@gmail.com>
2022-12-29 00:57:38 +0700

    design dasboard admin

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,7 +1,3 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
-    </div>
+<footer class="main-footer text-center">
+    <strong>Copyright &copy; 2023. All rights reserved.</strong>
 </footer>
